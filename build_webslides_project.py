7426274588e8747d994de8cc58bd570b95d7d61d
Duncan Nimmo <duncan@nimmo.co.nz>
2022-08-16 16:10:13 +1200

    added webslides build script

---
+++ build_webslides_project.py
@@ -0,0 +1,28 @@
+import os
+import requests
+
+os.mkdir("webslides_project")
+os.chdir("webslides_project")
+os.mkdir("images")
+os.mkdir("css")
+os.mkdir("js")
+
+
+files = { 
+	os.path.abspath('css/webslides.css'): 'https://raw.github.com/webslides/WebSlides/master/static/css/webslides.css',
+	os.path.abspath('js/webslides.js'): 'https://raw.github.com/webslides/WebSlides/master/static/js/webslides.js',
+	os.path.abspath('./index.html'): 'https://raw.github.com/nimmod/boilerplate/main/index.html'
+}
+
+
+for file in files:
+	fileName = os.path.basename(file)
+	r = requests.get(files[file])
+	text_file = open(file, "w")
+	print('requesting:' + files[file])
+	request = requests.get(files[file])
+	n = text_file.write(request.text)
+ 
+	#close file
+	text_file.close()
+
